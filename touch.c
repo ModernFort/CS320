2ad9f430ef753b602b54ca4d0aa794ec97c41f28
karl <255997651+ModernFort@users.noreply.github.com>
2026-02-02 14:30:30 -0800

    Touch - existing and new file
    Touch now working for:
    1. A pre-existing file
    2. A nonexistent file

--- touch.c
+++ touch.c
@@ -2,11 +2,26 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
 
 int touch(char *filepath) {
   int result;
-  if ((result = utimensat(AT_FDCWD, filepath, NULL, 0)) != 0) {
-    perror("Error:");
+  if ((result = access(filepath, F_OK)) < 0) {
+    if (errno == ENOENT) {
+      // requested file does not exist, ok to create
+      if ((result = creat(filepath, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH)) < 0) {
+        // file creation failed
+        perror("Create");
+        return result;
+      }
+    } else {
+      printf("%d\n", errno);
+      perror("Access");
+      return result;
+    }
+  } else if ((result = utimensat(AT_FDCWD, filepath, NULL, 0)) != 0) {
+    perror("Utimensat");
     return result;
   }
   return 0;
