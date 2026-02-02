0902323d956e8e108e3e62a4556d4b158f18aa09
karl <255997651+ModernFort@users.noreply.github.com>
2026-02-02 15:39:27 -0800

    Touch - options
    All options working independently and together

--- touch.c
+++ touch.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-int touch(char *filepath) {
+int touch(char *filepath, int opts) {
   int result;
   if ((result = access(filepath, F_OK)) < 0) {
     if (errno == ENOENT) {
@@ -20,9 +20,32 @@ int touch(char *filepath) {
       perror("Access");
       return result;
     }
-  } else if ((result = utimensat(AT_FDCWD, filepath, NULL, 0)) != 0) {
-    perror("Utimensat");
-    return result;
+  } else if (opts == 0) {
+    // touch or touch -am or touch -ma
+    if ((result = utimensat(AT_FDCWD, filepath, NULL, 0)) != 0) {
+      perror("Utimensat");
+      return result;
+    }
+  } else {
+    struct timespec acc;
+    struct timespec mod;
+
+    if (opts == 1) {
+      // touch -a
+      acc.tv_nsec = UTIME_NOW;
+      mod.tv_nsec = UTIME_OMIT;
+    } else {
+      // touch -m
+      acc.tv_nsec = UTIME_OMIT;
+      mod.tv_nsec = UTIME_NOW;
+    }
+
+    struct timespec times[] = {acc, mod};
+
+    if ((result = utimensat(AT_FDCWD, filepath, times, 0)) != 0) {
+      perror("Utimensat");
+      return result;
+    }
   }
   return 0;
 }
