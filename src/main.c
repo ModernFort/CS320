b51e68f6a26187231a05b33d1588465e3c02bc9a
karl <255997651+ModernFort@users.noreply.github.com>
2026-02-10 11:01:33 -0800

    Basic input handling
    Missing loop

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
+  printf("Enter a command: ");
+  fflush(stdout);
+  char command[PATH_MAX];
+  int result = scanf("%s", command);
+  if (result == EOF) {
+    if (ferror(stdin) != 0) {
+      perror("Scanf");
+      exit(errno);
+    }
+    printf("End of input received, exiting.\n");
+    exit(0);
+  } else if (result < 1) {
+    printf("Matching failed.\n");
+  }
   printf("Hello World!\n");
   exit(0);
 }
