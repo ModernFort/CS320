93055ac9996d60c8aeefe076f223afc23309db8f
joeman106 <joemanjc106@gmail.com>
2026-02-16 11:49:38 -0800

    Added delims for command tokenizing, defined a larger command buf max to include path max and the cmd itself

--- src/main.c
+++ src/main.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include <linux/limits.h>
 #include <errno.h>
+#include <string.h>
+
+//Delimiters to use for tokenizing user input, exclusively tokenizes on whitespace.
+#define TOKEN_DELIMS " \t\n"
+//Max length of a user command, including the path max plus a 64-byte buffer for the command itself.
+#define COMMAND_MAX PATH_MAX + 64
 
 int main(int argc, char *argv[]) {
-  char command[PATH_MAX];
+  char command_buf[COMMAND_MAX];
 
   while (1) {
     // read input
     printf("Enter a command: ");
     fflush(stdout);
-    int result = scanf("%s", command);
+    int result = scanf("%s", command_buf);
     if (result == EOF) {
       // EOF received
       if (ferror(stdin) != 0) {
@@ -18,6 +24,7 @@ int main(int argc, char *argv[]) {
         perror("Scanf");
         exit(errno);
       }
+
       // True EOF
       printf("End of input received, exiting.\n");
       exit(0);
