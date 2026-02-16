607d9d16ca98d036f764bfe0c41346a8f59f0e50
joeman106 <joemanjc106@gmail.com>
2026-02-16 12:01:34 -0800

    Added optional user flag to enable debugger

--- src/main.c
+++ src/main.c
@@ -8,9 +8,22 @@
 #define TOKEN_DELIMS " \t\n"
 //Max length of a user command, including the path max plus a 64-byte buffer for the command itself.
 #define COMMAND_MAX PATH_MAX + 64
+//Optional debug flag to enable debugging output
+#define DEBUG_FLAG "-d"
 
 int main(int argc, char *argv[]) {
   char command_buf[COMMAND_MAX];
+
+  int debug_mode = 0;
+  if(argc > 1){
+    if(strcmp(argv[1], DEBUG_FLAG) == 0){
+      debug_mode = 1;
+      printf("Debugger enabled!\n");
+    } else {
+      fprintf(stderr, "Error: flag \"%s\" not recognized\nRun \"./main -d\" to enable debugger output\n", argv[1]);
+      exit(EXIT_FAILURE);
+    }
+  }
 
   while (1) {
     // read input
@@ -25,13 +38,17 @@ int main(int argc, char *argv[]) {
         exit(errno);
       }
 
+      char* cmd_token = strtok(command_buf, TOKEN_DELIMS);
+      if(!cmd_token) continue;
+      
+
       // True EOF
       printf("End of input received, exiting.\n");
       exit(0);
     } else if (result < 1) {
       printf("Matching failed.\n");
     } else {
-      printf("Command %s received.\n", command);
+      printf("Command %s received.\n", command_buf);
       // add your commands as else if (condition) to trigger appropriate libraries
     }
   }
