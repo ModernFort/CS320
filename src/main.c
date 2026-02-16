c8842a3ed616593df214e534e582068b6458e978
joeman106 <joemanjc106@gmail.com>
2026-02-16 12:35:27 -0800

    Added tokenizer for multi-param input, puts params in an array

--- src/main.c
+++ src/main.c
@@ -8,12 +8,15 @@
 #define TOKEN_DELIMS " \t\n"
 //Max length of a user command, including the path max plus a 64-byte buffer for the command itself.
 #define COMMAND_MAX PATH_MAX + 64
+//Maximum amount of parameters that can be used for a command. Including flags, files, etc.
+#define MAX_PARAMS 64
 //Optional debug flag to enable debugging output
 #define DEBUG_FLAG "-d"
 
 int main(int argc, char *argv[]) {
   char command_buf[COMMAND_MAX];
 
+  //Check for any flags and enable debug mode if the debug flag is recognized.
   int debug_mode = 0;
   if(argc > 1){
     if(strcmp(argv[1], DEBUG_FLAG) == 0){
@@ -29,26 +32,42 @@ int main(int argc, char *argv[]) {
     // read input
     printf("Enter a command: ");
     fflush(stdout);
-    int result = scanf("%s", command_buf);
-    if (result == EOF) {
-      // EOF received
-      if (ferror(stdin) != 0) {
-        // read error
-        perror("Scanf");
-        exit(errno);
+    if (fgets(command_buf, COMMAND_MAX, stdin) == NULL) {
+      if (feof(stdin)) {
+          printf("End of input received, exiting.\n");
+          exit(0);
+      } else {
+          perror("fgets");
+          exit(EXIT_FAILURE);
+      }
+    } else {
+      char* cmd_token = strtok(command_buf, TOKEN_DELIMS);
+      if(!cmd_token) {
+        if(debug_mode) printf("DEBUGGER: No command entered. Continuing...\n");
+        continue;
       }
 
-      char* cmd_token = strtok(command_buf, TOKEN_DELIMS);
-      if(!cmd_token) continue;
-      
+      //Find all parameters of the user command (separated by whitespace) and put them in a null-terminated array.
+      char* cmd_params[MAX_PARAMS];
+      int param_idx = 0;
+      char* param = strtok(NULL, TOKEN_DELIMS);
+      while(param != NULL && param_idx < MAX_PARAMS){
+        cmd_params[param_idx] = param;
+        param = strtok(NULL, TOKEN_DELIMS);
+        param_idx++;
+      }
+      if(param_idx < MAX_PARAMS) cmd_params[param_idx] = NULL;
 
-      // True EOF
-      printf("End of input received, exiting.\n");
-      exit(0);
-    } else if (result < 1) {
-      printf("Matching failed.\n");
-    } else {
-      printf("Command %s received.\n", command_buf);
+      if(debug_mode) {
+        printf("DEBUGGER: Command %s entered ", cmd_token);
+        if(param_idx > 0){
+          printf("with params: ");
+          for(int i = 0; i < param_idx; i++){
+            printf("%s ", cmd_params[i]);
+          }
+          printf("\n");
+        }
+      }
       // add your commands as else if (condition) to trigger appropriate libraries
     }
   }
