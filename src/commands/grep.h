2b407e4811e9a880a61d062a5a9821c4623cf6df
joeman106 <joemanjc106@gmail.com>
2026-02-28 12:46:44 -0800

    Added several util function signitures in grep header

--- src/commands/grep.h
+++ src/commands/grep.h
@@ -6,6 +6,20 @@
 #ifndef GREP_H
 #define GREP_H
 
+//Returns an array of the valid flags that can be used with grep, to be used for param validation.
+char** get_valid_flags();
+
+//Takes an array of strings that represent the parameters the user used when using grep. Returns 0 if invalid params are
+//given, 1 if all params are valid.
+int validate_params(char** params);
+
+//Takes an array of strings representing arguments the user used when calling grep and parses them, setting relevant
+//global flags/variables like files and matching strings to be used in text/pattern matching later.
+int parse_args(char** args);
+
+//Takes a string as an argument and returns the version with all lowercase, used if ignoring case when checking match.
+char* lower_line(const char* line);
+
 //Open the file desired by the user, returning the open file pointer
 FILE* open_file(char* user_file);
 
@@ -14,6 +28,10 @@ int match_text(FILE* fp, const char* str_to_match);
 
 //Search a file for an extended regex and print lines that match to stdout
 int match_pattern(FILE* fp, const char* pattern_to_match);
+
+//Simple util function that takes a line from a file and uses a sliding window to determine if the line
+//Contains the given string. returns 1 if the string is contained, 0 if not (for easy boolean checks)
+int contains_text(const char* line, const char* str_to_match);
 
 //TODO: Implement match functionality for things other than file pointers, like piped output through dynamically created files (EG through cat)
 #endif
