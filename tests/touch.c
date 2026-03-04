bea2c3627cb7d580414bd34e56d867df086e7ecf
karl <255997651+ModernFort@users.noreply.github.com>
2026-03-04 14:28:36 -0800

    Black Box: No file provided to touch

--- tests/touch.c
+++ tests/touch.c
@@ -2,6 +2,24 @@
 #include <CUnit/CUnit.h>
 #include <../src/touch.h>
 
+void no_file() {
+  CU_ASSERT_EQUAL(touch([], 0), -1);
+  CU_ASSERT_EQUAL(touch(["-a"], 1), -1);
+  CU_ASSERT_EQUAL(touch(["-m"], 1), -1);
+  CU_ASSERT_EQUAL(touch(["-am"], 1), -1);
+  CU_ASSERT_EQUAL(touch(["-ma"], 1), -1);
+  CU_ASSERT_EQUAL(touch(["-a", "-m"], 2), -1);
+  CU_ASSERT_EQUAL(touch(["-m", "-a"], 2), -1);
+}
+
+void black_box() {
+  no_file();
+  // file exists
+  // file does not exist
+  // file accessible
+  // file not accessible
+}
+
 int main() {
   CU_initialize_registry();
 
@@ -9,18 +27,6 @@ int main() {
   CU_pSuite whiteBox = CU_add_suite("White Box Tests", NULL, NULL);
   CU_pSuite integration = CU_add_suite("Integration Tests", NULL, NULL);
   // black box test cases:
-  // possible inputs:
-  // touch
-  // touch -a
-  // touch -am
-  // touch -ma
-  // touch -m
-  // touch -m -a
-  // touch -a -m
-  // file exists
-  // file does not exist
-  // file accessible
-  // file not accessible
   // run tests
   // cleanup registry
   return CU_get_error();
