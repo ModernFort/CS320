fb4ad22dc5777334fc0aaa2e9610df7ef205268a
karl <255997651+ModernFort@users.noreply.github.com>
2026-03-04 15:21:04 -0800

    no_file() test complete

--- tests/touch.c
+++ tests/touch.c
@@ -1,15 +1,21 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
-#include <../src/touch.h>
+#include "../src/commands/touch.h"
 
 void no_file() {
-  CU_ASSERT_EQUAL(touch([], 0), -1);
-  CU_ASSERT_EQUAL(touch(["-a"], 1), -1);
-  CU_ASSERT_EQUAL(touch(["-m"], 1), -1);
-  CU_ASSERT_EQUAL(touch(["-am"], 1), -1);
-  CU_ASSERT_EQUAL(touch(["-ma"], 1), -1);
-  CU_ASSERT_EQUAL(touch(["-a", "-m"], 2), -1);
-  CU_ASSERT_EQUAL(touch(["-m", "-a"], 2), -1);
+  CU_ASSERT_EQUAL(touch(NULL, 0), -1);
+  char *a[] = {"-a"};
+  CU_ASSERT_EQUAL(touch(*a, 1), -1);
+  char *m[] = {"-m"};
+  CU_ASSERT_EQUAL(touch(*m, 1), -1);
+  char *am[] = {"-am"};
+  CU_ASSERT_EQUAL(touch(*am, 1), -1);
+  char *ma[] = {"-ma"};
+  CU_ASSERT_EQUAL(touch(*ma, 1), -1);
+  char *am_sep[] = {"-a", "-m"};
+  CU_ASSERT_EQUAL(touch(*am_sep, 2), -1);
+  char *ma_sep[] = {"-m", "-a"};
+  CU_ASSERT_EQUAL(touch(*ma_sep, 2), -1);
 }
 
 void black_box() {
