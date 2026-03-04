0dc9d11f44b1814fce185c11ea3fcfc86e77923b
karl <255997651+ModernFort@users.noreply.github.com>
2026-03-04 13:57:32 -0800

    Basic test template

---
+++ tests/touch.c
@@ -0,0 +1,11 @@
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+int main() {
+  // intialize registry
+  // create suite
+  // add tests
+  // run tests
+  // cleanup registry
+  return CU_get_error();
+}
