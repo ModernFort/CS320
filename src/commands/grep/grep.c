f0c40a75bc43e6227d3285a2fba863c9c2c88c0f
joeman106 <joemanjc106@gmail.com>
2026-03-29 14:13:49 -0700

    Added main text match function to the correct file, updated to accept a reference to the current state

--- src/commands/grep/grep.c
+++ src/commands/grep/grep.c
@@ -3,27 +3,6 @@
 //Maximum length a line of a file can be, equivalent to the max page size on most systems.
 #define MAX_LINE_LEN 4096
 
-int match_text(FILE* fp, const char* str_to_match){
-    //Create a buffer for fgets to store the line of the file
-    char buf[MAX_LINE_LEN];
-
-    int found_match = 0;
-
-    //Iterate over each line of the file until EOF is reached
-    while(fgets(buf, MAX_LINE_LEN, fp) != NULL){
-        int len = strlen(buf);
-        //If the text is contained, print the entire line (grep's default functionality)
-        if(contains_text(buf, str_to_match)){
-            found_match = 1;
-            printf("%s", buf);
-            //If the line in the file somehow didn't contain a newline, manually add one.
-            if(strlen(buf) != 0 && buf[len - 1] != '\n') putchar('\n');
-        }
-    }
-
-    return found_match;
-}
-
 int match_pattern(FILE* fp, const char* pattern_to_match){
 
 }
--- src/commands/grep/grep_functions/text_matching.c
+++ src/commands/grep/grep_functions/text_matching.c
@@ -1,5 +1,26 @@
 #include "text_matching.h"
 
+int match_text(FILE* fp, const char* str_to_match, grep_state current_state){
+    //Create a buffer for fgets to store the line of the file
+    char buf[MAX_LINE_LEN];
+
+    int found_match = 0;
+
+    //Iterate over each line of the file until EOF is reached
+    while(fgets(buf, MAX_LINE_LEN, fp) != NULL){
+        int len = strlen(buf);
+        //If the text is contained, print the entire line (grep's default functionality)
+        if(contains_text(buf, str_to_match)){
+            found_match = 1;
+            printf("%s", buf);
+            //If the line in the file somehow didn't contain a newline, manually add one.
+            if(strlen(buf) != 0 && buf[len - 1] != '\n') putchar('\n');
+        }
+    }
+
+    return found_match;
+}
+
 int contains_text(char* line, const char* str_to_match){
     //Use strstr to check the line for the substring, returning 0 if the string
     //is not found, in which case strstr returns NULL.
