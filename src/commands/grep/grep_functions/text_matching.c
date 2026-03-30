e9542e2a29442af29798a337513aafeb4d805bf5
joeman106 <joemanjc106@gmail.com>
2026-03-30 13:50:33 -0700

    commiting a minor spacing change to get git to shut up

--- src/commands/grep/grep_functions/text_matching.c
+++ src/commands/grep/grep_functions/text_matching.c
@@ -16,6 +16,8 @@ char* match_next_line(FILE* fp, const char* str_to_match, int ignore_case){
     return buf;
 }
 
+
+
 int contains_text(char* line, const char* str_to_match){
     //Use strstr to check the line for the substring, returning 0 if the string
     //is not found, in which case strstr returns NULL.
