ca225dd945184bb61be13c41963f0bcf27bb105d
joeman106 <joemanjc106@gmail.com>
2026-03-30 12:38:22 -0700

    Changed state struct to static flags/settings struct, made new struct to store running totals

--- src/commands/grep/grep.h
+++ src/commands/grep/grep.h
@@ -9,9 +9,13 @@
 
 #include "grep_util/grep_state.h"
 #include "grep_functions/text_matching.h"
+#include "grep_util/grep_util.h"
 
 //Search a file for an extended regex and print lines that match to stdout
 int match_pattern(FILE* fp, const char* pattern_to_match);
+
+//Match text from all files according to the state of the command
+void match_files_txt(grep_state *state);
 
 //TODO: Implement match functionality for things other than file pointers, like piped output through dynamically created files (EG through cat)
 #endif
