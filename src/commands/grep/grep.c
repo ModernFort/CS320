b6fda0aa2b42380c207fb368218070da35b90730
joeman106 <joemanjc106@gmail.com>
2026-04-02 14:20:10 -0700

    Fixed a few compilation bugs

--- src/commands/grep/grep.c
+++ src/commands/grep/grep.c
@@ -19,9 +19,6 @@ void match_files_txt(grep_state *state){
     char** files = state->file_info.file_paths;
     int file_count = state->file_info.file_count;
 
-    char** patterns = state->pattern_info.patterns;
-    int pattern_count = state->pattern_info.pattern_count;
-
     FILE* current_fp;
 
     for(int i = 0; i < file_count; i++){
@@ -29,5 +26,11 @@ void match_files_txt(grep_state *state){
         current_fp = open_file(files[i]);
         match_file_txt(state, current_fp, files[i]);
     }
+}
 
+int main(int argc, char** argv){
+    char** grep_args = argv+1;
+    grep_state state = get_state(argc-1, grep_args);
+    match_files_txt(&state);
+    return 0;
 }
--- src/commands/grep/grep_util/grep_flags.c
+++ src/commands/grep/grep_util/grep_flags.c
@@ -1,6 +1,29 @@
 #include "grep_flags.h"
 #include "stdio.h"
 
+ const char *VALID_FLAGS[] = {
+    //Pattern matching flags
+    "-E", "--extended-regexp", //Flags to indicate extended regex matching
+    "-F", "--fixed-strings", //Indicates plain text matching
+    "-G", "--basic-regexp", //Basic regex matching
+    
+    //Matching control flags
+    "-e", "--regexp=", //Searches for multiple patterns
+    "-f", "--file=", //Matches a specific file
+    "-i", "--ignore-case", //Ignore case of pattern/input data
+    "-v", "--invert-match", //Select only non-matching lines
+
+    //Output control flags
+    "-c", "--count", //Print a count of matching lines rather than normal output
+    "-L", "--files-without-match", //Print the name of input files without matching text
+    "-l", "--files-with-matches", //Print files that do contain matching text
+    "-m", "--max-count=", //Stop reading a file after --max-count= n matching lines
+    "-o", "--only-matching", //Print only the matching parts of a line seperate from the actual line
+    "-s", "--no-messages" //Suppress error messages about unreadable/nonexistent files 
+};
+
+const int VALID_FLAG_COUNT = sizeof(VALID_FLAGS) / sizeof(VALID_FLAGS[0]);
+
 int flag_valid(const char* param){
     if(param == NULL) return 0;
     //Iterate over the array of valid flags, if a match is found return 1.
