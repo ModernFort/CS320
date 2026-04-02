fb7d2beb438947ed3abd47f6de5cb6421dd5b76f
joeman106 <joemanjc106@gmail.com>
2026-04-02 14:08:01 -0700

    Updated main to use match files helper

--- src/commands/grep/grep.c
+++ src/commands/grep/grep.c
@@ -25,26 +25,9 @@ void match_files_txt(grep_state *state){
     FILE* current_fp;
 
     for(int i = 0; i < file_count; i++){
-        //Open the file and initialize a variable to store the cap for the getline wrapper
-        //and the line itself
+        //Open the file and match it using the helper
         current_fp = open_file(files[i]);
-        char *line = NULL;
-        size_t cap = 0;
-
-        //Read each line of the file until EOF is returned
-        while(read_next_line(current_fp, &line, &cap) != -1){
-            //Iterate over the patterns of the array and compare each to each
-            //line of the file
-            for(int j = 0; j < pattern_count; j++){
-                if(contains_text(line, patterns[j])){
-                    printf("%s", line);
-                    break;
-                }
-            }
-        }
-
-        free(line);
-        fclose(current_fp);
+        match_file_txt(state, current_fp, files[i]);
     }
 
 }
