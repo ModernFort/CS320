7e828bb80b5c28d8484e6f21549de7a318cff349
Guy-an <guyanhd@gmail.com>
2026-04-20 05:17:26 +0000

    add functionality to create_new_description function

--- src/commands/manpage/manpage.c
+++ src/commands/manpage/manpage.c
@@ -1,4 +1,5 @@
 #include "manpage.h"
+#include <errno.h>
 
 FILE* get_manpage_file(char* query){
     char fileloc[strlen(query)+strlen("descriptions/")+1];
@@ -9,6 +10,15 @@ FILE* get_manpage_file(char* query){
 }
 
 int create_new_description(char* query, char* description){
+    FILE* existing_file = get_manpage_file(query);
+    if (existing_file) return -1;
+    
+    char new_file[strlen(query)+strlen("descriptions/")+1];
+    strcpy(new_file, "descriptions/");
+    strcat(new_file, query);
+    FILE* fileloc = fopen(new_file, "w");
+    fwrite(description, sizeof(char), strlen(description), fileloc);
+    fclose(fileloc);
     return 0;
 }
 
@@ -49,6 +59,38 @@ int request_manpage(char* query) {
     return 0;
 }
 
+/*
 int delete_description(char* query){
+    FILE* del_file = get_manpage_file(query);
+    printf("1\n");
+    // Checking if the file exists first
+    if (!del_file) return -1; 
+    printf("2\n");
+
+    // Getting the file destination
+    printf("3\n");
+
+    char filedest[strlen(query)+strlen("descriptions/deleted/")+1];
+    strcpy(filedest, "descriptions/deleted/");
+    strcat(filedest, query);
+    printf("4\n");
+
+    // Duplicate the file into deleted and delete the current file
+    FILE* del_file_binloc = fopen(filedest, "w");
+    int bufferSize = 5;
+    char buffer[bufferSize];
+    printf("5\n");
+
+    while ((fread(buffer, sizeof(char), bufferSize-1, del_file) > 0)) {
+        fwrite(buffer, sizeof(char), bufferSize-1, del_file_binloc);
+    }
+
+    printf("6\n");
+    fclose(del_file);
+    remove(del_file);
+    fclose(del_file_binloc);
+    printf("7\n");
+
     return 0;
 }
+    */
